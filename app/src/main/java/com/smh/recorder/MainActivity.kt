878a506c8bce92699b2adf64c0232bd80d62ed5e
MinhyeokSun <s.minyeok@gmail.com>
2021-12-19 22:55:58 +0900

    4th Commit

--- app/src/main/java/com/smh/recorder/MainActivity.kt
+++ app/src/main/java/com/smh/recorder/MainActivity.kt
@@ -2,8 +2,12 @@ package com.smh.recorder
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaParser
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +15,23 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.recordButton)
     }
 
-    private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
+    private val requiredPermissions = arrayOf(
+        Manifest.permission.RECORD_AUDIO,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
 
+    private val recordingFilePath: String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
     private var state = State.BEFORE_RECORDING
+
+    set(value) {
+        field = value
+        recordButton.updateIconWithState(value)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +39,7 @@ class MainActivity : AppCompatActivity() {
 
         requestAudioPermission() // 권한 요청
         initViews()
+        bindViews()
     }
 
     override fun onRequestPermissionsResult(
@@ -46,6 +65,64 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         recordButton.updateIconWithState(state)
+    }
+
+    private fun bindViews() {
+        recordButton.setOnClickListener {
+            when(state) {
+                State.BEFORE_RECORDING -> {
+                    startRecording()
+                }
+
+                State.ON_RECORDING -> {
+                    stopRecording()
+                }
+
+                State.AFTER_RECORDING -> {
+                    startPlaying()
+                }
+
+                State.ON_PLAYING -> {
+                    stopPlaying()
+                }
+            }
+        }
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+            setOutputFile(recordingFilePath)
+            prepare()
+        }
+        recorder?.start()
+        state = State.ON_RECORDING
+    }
+
+    private fun stopRecording() {
+        recorder?.run{
+            stop()
+            release() // 메모리 해제
+        }
+        recorder = null
+        state = State.AFTER_RECORDING
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            setDataSource(recordingFilePath)
+            prepare() // 스트리밍 혹은 오래걸리면 asyncprepare 쓰기. prepare 쓰면 앱이 잠깐 멈출 수 있음.
+        }
+        player?.start()
+        state = State.ON_PLAYING
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+        state = State.AFTER_RECORDING
     }
 
     companion object {
